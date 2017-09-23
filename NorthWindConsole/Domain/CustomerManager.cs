1620306764e06eaf38907d6d41c02e1237b77f9c
Soji Egbonwon <sojiegbonwon@yahoo.com>
2017-09-23 17:47:41 +0100

    initial code

---
+++ NorthWindConsole/Domain/CustomerManager.cs
@@ -0,0 +1,65 @@
+﻿using NorthWindConsole.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWindConsole.Domain
+{
+    /// <summary>
+    /// customer-related operations
+    /// </summary>
+    public class CustomerManager
+    {
+        private NORTHWNDEntities _dbContext;
+
+        public CustomerManager()
+        {
+            //initialization codes
+            _dbContext = new NORTHWNDEntities();
+        }
+
+        public List<Customer> GetCustomers()
+        {
+            var data = _dbContext.Customers.ToList();
+            return data;
+        }
+
+        public Customer GetCustomerById(string id)
+        {
+            var data = _dbContext.Customers.FirstOrDefault(p => p.CustomerID == id);
+            return data;
+        }
+
+        public string AddCustomer(Customer customer)
+        {
+            var newcustomer =_dbContext.Customers.Add(customer);
+            _dbContext.SaveChanges();
+            if (newcustomer != null)
+                return customer.CustomerID;
+            else
+                return null;
+        }
+
+        public bool UpdateCustomer(Customer customer)
+        {
+            _dbContext.Entry(customer).State = System.Data.Entity.EntityState.Modified;
+            int result = _dbContext.SaveChanges();
+            if (result > 0)
+                return true;
+            else
+                return false;
+            
+        }
+
+        public List<Customer> SearchCustomersById(string id)
+        {
+            var customers = _dbContext.Customers.Where(p => p.CustomerID.Contains(id));
+            if (customers != null)
+                return customers.ToList();
+            else
+                return null;
+        }
+    }
+}
---
+++ NorthWindConsole/Program.cs
@@ -0,0 +1,70 @@
+﻿using NorthWindConsole.Data;
+using NorthWindConsole.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWindConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to NorthWind e-Commerce");
+            Console.WriteLine("Enter 1 for Customers \nEnter 2 to search by customer id \nEnter 3 to get a customer by id \nEnter 4 for Products List");
+
+            string input = Console.ReadLine();
+            if (input != null)
+            {
+                input = input.Trim(); //trim
+                if (input == "1")
+                {
+                    GetCustomers();
+                }
+                else if (input == "2")
+                {
+                    Console.WriteLine("Enter customer id you want to search by");
+                    var query = Console.ReadLine();
+                    GetCustomers(query);
+                }
+                else if (input == "3")
+                {
+                    Console.WriteLine("Enter customer id");
+                    var id = Console.ReadLine();
+                    GetCustomer(id);
+                }
+            }
+            Console.ReadKey();
+        }
+
+        private static void GetCustomers(string id = null)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            List<Customer> customers = null;
+            if (string.IsNullOrEmpty(id))
+                customers = customerManager.GetCustomers();
+            else
+                customers = customerManager.SearchCustomersById(id);
+
+            foreach(Customer customer in customers)
+            {
+                Console.WriteLine($"{customer.CustomerID} {customer.ContactName} {customer.Phone} \n");
+                //Console.WriteLine("{0} {1} {2}",customer.CustomerID, customer.ContactName, customer.Phone);
+            }
+        }
+
+        private static void GetCustomer(string id)
+        {
+            CustomerManager customerManager = new CustomerManager();
+            var customer = customerManager.GetCustomerById(id);
+            if (customer != null)
+                Console.WriteLine($"{customer.CustomerID} {customer.ContactName} {customer.Phone} \n");
+            else
+                Console.WriteLine($"Cannot find customer with id {id}");
+        }
+
+
+    }
+}
